9b5ef6df14862e45279f1cb1c166cdd54e792090
Ai Xin <aixin@ctgu.edu.cn>
2015-12-18 20:05:13 +0800

    将搜索功能封装为函数，并实现提取多页结果，仅将搜索标题输出

--- CrawlerBaidu.py
+++ CrawlerBaidu.py
@@ -5,39 +5,33 @@ import urllib2
 import re
 from bs4 import BeautifulSoup as BS
 
-baseUrl = 'http://www.baidu.com/s'
-page = 1 #第几页
-word = '穿戴设备'  #搜索关键词
+def search(word, startPageNum, endPageNum, fileName, writeMode):
+    baseUrl = 'http://www.baidu.com/s'
+    page = 1 #第几页
+    #word = '穿戴设备'  #搜索关键词
+    file = open(fileName,writeMode)
 
-data = {'wd':word,'pn':str(page-1)+'0','tn':'baidurt','ie':'utf-8','bsst':'1'}
-data = urllib.urlencode(data)
-url = baseUrl+'?'+data
+    for page in range(startPageNum,endPageNum+1):
+        data = {'wd':word,'pn':str(page-1)+'0','tn':'baidurt','ie':'utf-8','bsst':'1'}
+        data = urllib.urlencode(data)
+        url = baseUrl+'?'+data
 
-try:
-    request = urllib2.Request(url)
-    response = urllib2.urlopen(request)
-except urllib2.HttpError,e:
-    print e.code
-    exit(0)
-except urllib2.URLError,e:
-    print e.reason
-    exit(0)
+        try:
+            request = urllib2.Request(url)
+            response = urllib2.urlopen(request)
+        except urllib2.HttpError,e:
+            print e.code
+            exit(0)
+        except urllib2.URLError,e:
+            print e.reason
+            exit(0)
 
-html = response.read()
-soup = BS(html)
-td = soup.find_all(class_='f')
+        html = response.read()
+        soup = BS(html)
+        td = soup.find_all(class_='f')
 
-for t in td:
-    print t.h3.a.get_text()
-    print t.h3.a['href']
+        for t in td:
+            titleStr = t.h3.a.get_text()+u'\n'
+            file.write(titleStr.encode('GBK'))
     
-    font_str = t.find_all('font',attrs={'size':'-1'})[0].get_text()
-    start = 0 #起始
-    realtime = t.find_all('div',attrs={'class':'realtime'})
-    if realtime:
-        realtime_str = realtime[0].get_text()
-        start = len(realtime_str)
-        print realtime_str
-    end = font_str.find('...')
-    print font_str[start:end+3],'\n'
-    
+search('宜昌',1,3,u'yichang.txt','w')
